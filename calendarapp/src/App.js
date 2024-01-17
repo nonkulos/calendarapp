79158b792d6ad5769c5134c4f29048426977afdd
nonkulos <nicholasnguyen1129@gmail.com>
2024-01-16 23:48:36 -0800

    Another test commit

--- calendarapp/src/App.js
+++ calendarapp/src/App.js
@@ -10,10 +10,7 @@ function App() {
       <Boxes />
       <Boxes />
       <Boxes />
-<<<<<<< HEAD
       <Boxes />
-=======
->>>>>>> d12fb2a9f575ac46b0904f976cf89af83f28418b
     </table>
   );
 }
