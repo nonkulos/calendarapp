4fbc03ce36a4b92390008d7e26b1ff7680e5baac
nonkulos <nicholasnguyen1129@gmail.com>
2024-01-16 23:04:00 -0800

    First commit; test

---
+++ calendarapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const Boxes = () => {
+  return (<td>ssldkfj</td>)
+}
+
+function App() {
+  return (
+    <table>
+      <Boxes />
+      <Boxes />
+    </table>
+  );
+}
+
+export default App;
