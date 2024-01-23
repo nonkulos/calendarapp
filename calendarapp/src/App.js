906777615f9e73172f1ff2c201ca89e9137015fc
Nicholas Nguyen <140012838+nonkulos@users.noreply.github.com>
2024-01-23 06:47:58 +0000

    Used array.map() yey

--- calendarapp/src/App.js
+++ calendarapp/src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 
-const Boxes = () => {
-  return (<td>ssldkfj</td>)
+const Boxes = ({number}) => {
+  const rows = calcNum(number)
+  const days = rows.map(x => <td>{x}</td>);
+  return <tr>{days}</tr>;
+}
+
+function calcNum(number) {
+  const rows = [];
+  for(let i = 0; i < 7; i++) {
+    rows.push(number + i)
+  }
+  return rows
 }
 
 function App() {
   return (
-    <>
-      <table>
-        <Boxes />
-        <Boxes />
-        <Boxes />
-      </table>
-
-      <table>
-        <Boxes />
-        <Boxes />
-        <Boxes />
-      </table>
-    </>
+    <table>
+      <tr>
+        <Boxes number = {1} />
+      </tr>
+      <tr>
+        <Boxes number = {8} />
+      </tr>
+    </table>
   );
 }
 
