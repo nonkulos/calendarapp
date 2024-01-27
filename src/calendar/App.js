921492b9530edf46d34b9c5bc7605030e149e2af
Nicholas Nguyen <140012838+nonkulos@users.noreply.github.com>
2024-01-27 00:46:16 +0000

    Changed number of weeks displayed from 5 to 6.

--- src/calendar/App.js
+++ src/calendar/App.js
@@ -1,20 +1,15 @@
 import './App.css';
 import calcNum from "./getdates.js"
 
-const Boxes = ({number}) => {
-  const rows = calcNum(number)
-  const days = rows.map(x => <td>{x}</td>);
-  return <tr>{days}</tr>;
+const Calendar = () => {
+  const rows = calcNum()
+  return <table>{rows}</table>;
 }
 
 const App = () => {
   return (
     <table>
-        <Boxes number = {1} />
-        <Boxes number = {8} />
-        <Boxes number = {15} />
-        <Boxes number = {22} />
-        <Boxes number = {29} />
+        <Calendar />
     </table>
   );
 }
--- src/calendar/getdates.js
+++ src/calendar/getdates.js
@@ -1,13 +1,27 @@
-const calcNum = (number) => {
+const calcNum = () => {
+  const numweeks = 6
+  const numdays = 7
   let day = new Date();
-  day.setDate(number)
+  day.setDate(0)
+  day.setDate(1)
   day.setDate(day.getDate() - day.getDay() - 1);
-  let weekArr = []
-  for(let i = 0; i < 7; i++){
-    day.setDate(day.getDate() + 1)
-    weekArr.push(day.getDate())
+  let week = [[], [], [], [], [], []];
+ 
+  for(let i = 0; i < numweeks; i++){
+    for(let j = 0; j < numdays; j++){
+      day.setDate(day.getDate() + 1)
+      week[i].push(<td>{day.getDate()}</td>)
+    }
   }
-  return weekArr;
+  return (
+    <table>
+      <tr>{week[0]}</tr>
+      <tr>{week[1]}</tr>
+      <tr>{week[2]}</tr>
+      <tr>{week[3]}</tr>
+      <tr>{week[4]}</tr>
+      <tr>{week[5]}</tr>
+    </table>);
 }
 
 export default calcNum;
