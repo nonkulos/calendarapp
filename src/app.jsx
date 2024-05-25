60dab8ea6a7031144d4a165e49148a0ac073e964
Nicholas <nicholasnguyen1129@gmail.com>
2024-05-25 15:13:39 -0700

    Created empty express.js skeleton and connected to React app. Updated .gitignore file.

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,23 @@
+import React, { useState, useEffect } from "react";
+
 import Buttons from './components/calendar/adjMonth';
 import Sidebar from './components/sidebar/sidebar';
 import Bar from './components/topBar/barContents';
 import EventViewer from './components/eventViewer/eventViewer';
 
 const App = () => {
+    
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
     return (
         <>
+            <h1>{message}</h1>
             <div className="topbar">
                 <Bar />
             </div>
