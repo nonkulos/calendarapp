562fb6c74daa7a015c4bea2341f9504f5a62aa9f
Nicholas <nicholasnguyen1129@gmail.com>
2024-06-20 18:40:06 -0700

    Days that are not in the current month are now greyed out.

--- src/components/calendar/createCalendar.jsx
+++ src/components/calendar/createCalendar.jsx
@@ -2,7 +2,7 @@ import showEventViewer from "../../style-stuff/logic/toggleEventViewer";
 
 const calcNum = (year, month) => {
   let day = new Date(year, month);
-  day.setDate(1)
+  day.setDate(1);
   day.setDate(day.getDate() - day.getDay() - 1);
 
   const toggleEventViewer = (event) => {
@@ -30,12 +30,21 @@ const calcNum = (year, month) => {
         weeks.map((week, i) => (
           <tr key={i}>
             {
-              week.map((day, j) => 
+              week.map((day, j) => (
+                // If the day is not in the current month, color it grey
+                i == 0 && day > 7 || i >= 4 && day < 23 ?
+                  <td key={j} className="calendarNum grey" onClick={toggleEventViewer} id = {((5-i)*7)+(7-j)-1}>
+                    {day}
+                    <div className="dotTemplate dot" />
+                    <div className="dotTemplate dot" />
+                  </td>
+                  :
                   <td key={j} className="calendarNum" onClick={toggleEventViewer} id = {((5-i)*7)+(7-j)-1}>
                     {day}
                     <div className="dotTemplate dot" />
                     <div className="dotTemplate dot" />
                   </td>
+                )
               )
             }
           </tr>
