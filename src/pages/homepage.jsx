3d47c9155b0b35abfd4befdb6ae85a338ab0f4e5
Nicholas <nicholasnguyen1129@gmail.com>
2024-07-28 21:57:09 -0700

    fix: modified package.json and server code to not use localhost

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -31,7 +31,7 @@ const Homepage = () => {
     const [message, setMessage] = useState("");
 
     useEffect(() => {
-    fetch("http://localhost:3001/message")
+    fetch("https://calendarapp-production-cb6a.up.railway.app/message")
         .then((res) => res.json())
         .then((data) => setMessage(data.message));
     }, []);
