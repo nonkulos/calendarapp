ff7fd58a9d48a7625660f738e301739b3056af0d
Nicholas <nicholasnguyen1129@gmail.com>
2024-07-29 15:54:49 -0700

    Added dockerfile. Changed api path.

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:lts-slim
+WORKDIR /calendarapp
+COPY public/ /calendarapp/public
+COPY src/ /calendarapp/src
+COPY package.json /calendarapp
+RUN npm install
+CMD ["npm", "start"]
+
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/message', (req, res) => {
+app.get('/', (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
