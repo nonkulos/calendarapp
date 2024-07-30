78acf7464f03be5b6b19de34d28685bdaff95fd8
Nicholas <nicholasnguyen1129@gmail.com>
2024-07-30 16:19:27 -0700

    Moved app.listen to bottom of server.js.

--- server/server.js
+++ server/server.js
@@ -5,24 +5,26 @@ import cors from "cors";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const publicPath = path.join(__dirname, '..', 'public'); // this path expects that you put the server file inside of a folder, such as server. If you want to make it in the top level directory, get rid of '..'
+app.use(express.static(publicPath));
+
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ message: "Hello from server!" });
+    res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.listen(3001, "0.0.0.0", () => {
-    console.log(`Server is running on port 3001.`);
-  });
 
 app.post('/formSubmit', (req, res) => {
     console.log("post request received");
     res.sendStatus(201)
 });
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.listen(3001, "0.0.0.0", () => {
+    console.log(`Server is running on port 3001.`);
+  });
 
-const publicPath = path.join(__dirname, '..', 'public'); // this path expects that you put the server file inside of a folder, such as server. If you want to make it in the top level directory, get rid of '..'
-app.use(express.static(publicPath));
